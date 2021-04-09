1d227f53225c29aa9aa613f3426e6cb6bd27c90d
LPascholatti <lucaspascholatti@gmail.com>
2021-04-09 16:01:10 +0200

    Fix syntax for test_models, test passing

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,8 +6,8 @@ class ModelTest(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
-        email = "testemail@provider.com",
-        password = "Testpass123#",
+        email = "testemail@provider.com"
+        password = "Testpass123#"
         user = get_user_model().objects.create_user(
             email=email,
             password=password
