c7582754255c5e727c07c88e889cab489890ad4c
LPascholatti <lucaspascholatti@gmail.com>
2021-04-26 15:04:55 +0200

    Commit test_admin test and admin, which registers a basic UserAdmin that displays a list of users per id

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,27 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@provider.com",
+            password="Password1234!"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="user@provider.com",
+            password="Password1234!",
+            name="User Lastname"
+        )
+
+    def test_users_listed(self):
+        """Test users are listed in the user's page"""
+        url = reverse("admin:core_user_changelist")
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
